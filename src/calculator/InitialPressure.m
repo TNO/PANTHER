c8a7e045e5110594abb2d656c945bf9cf77f5514
Buijze <loes.buijze@tno.nl>
2023-05-11 17:31:17 +0200

    Fixed bug in setting initial pressure gradient

--- src/calculator/InitialPressure.m
+++ src/calculator/InitialPressure.m
@@ -31,13 +31,14 @@ classdef InitialPressure
             p0_HW(top_HW_i:end) = p0_HW(top_HW_i:end) + member.p_over;  
             p0_FW(top_FW_i:end) = p0_FW(top_FW_i:end) + member.p_over;  
             % set buoyancy pressure gradient within the reservoir compartments
-            p0_FW(next_to_FW) = p0_FW(base_FW_i) + (yy(next_to_FW) - yy(base_FW_i))*member.p_grad_res/1000;
-            p0_HW(next_to_HW) = p0_HW(base_HW_i) + (yy(next_to_HW) - yy(base_HW_i))*member.p_grad_res/1000; 
+            p0_FW(next_to_FW) = p0_FW(base_FW_i) - (yy(next_to_FW) - yy(base_FW_i))*member.p_grad_res/1000;
+            p0_HW(next_to_HW) = p0_HW(base_HW_i) - (yy(next_to_HW) - yy(base_HW_i))*member.p_grad_res/1000; 
             % set fault pressure
             if strcmp(p_fault_mode, 'max')
                 p0 = max(p0_HW, p0_FW);
             elseif strcmp(p_fault_mode, 'min')
                 p0 = min(p0_HW, p0_FW);
+                p0 = max(p0_HW, p0_FW); % TODO fix bug here, for initial pressure it should be the max p 
             elseif strcmp(p_fault_mode, 'mean')
                 p0 = mean([p0_HW, p0_FW],2);
             elseif strcmp(p_fault_mode, 'FW')
